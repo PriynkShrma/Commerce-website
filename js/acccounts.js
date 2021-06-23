4b6aafbc856359a39de9c36424d2d259ab435ad1
priyank <444priyank@gmail.com>
2021-06-24 01:28:44 +0545

    initial commit

---
+++ js/acccounts.js
@@ -0,0 +1,120 @@
+
+
+// for ch1 
+let bn=document.getElementById('bn');
+bn.addEventListener('click',view)
+
+
+function view(){
+    let para=document.getElementById('ch1');
+    if(para.style.display = 'none'){
+        para.style.display ='block';
+        
+    }
+    else{
+       
+    }
+}
+function Hide(){
+    let btn = document.getElementById('hide1');
+    let para=document.getElementById('ch1');
+     if(para.style.display != 'none'){
+         para.style.display ='none';
+         return 0 ;
+     }
+     else{
+    
+     }
+}
+
+
+
+
+
+
+
+
+
+function ViewVideo(){
+    let para=document.getElementById('firstvideos');
+    if(para.style.display = 'none'){
+        para.style.display ='flex';
+        
+    }
+    else{
+       
+    }
+}
+function HideVideo(){
+    let btn = document.getElementById('hv');
+    let para=document.getElementById('firstvideos');
+     if(para.style.display != 'none'){
+         para.style.display ='none';
+         return 0 ;
+     }
+     else{
+    
+     }
+}
+     
+
+
+
+
+
+// for ch 2
+// let bnt=document.getElementById('bn2');
+// bnt.addEventListener('click',viewt)
+
+
+function viewt(){
+    let para=document.getElementById('ch2');
+    if(para.style.display = 'none'){
+        para.style.display ='block';
+        
+    }
+    else{
+       
+    }
+}
+
+function Hidet(){
+    
+    let para=document.getElementById('ch2');
+     if(para.style.display != 'none'){
+         para.style.display ='none';
+         return 0 ;
+     }
+     else{
+    
+     }
+    }
+
+
+
+
+
+
+
+    function ViewVideos(){
+        let para=document.getElementById('firstvideos2');
+        if(para.style.display = 'none'){
+            para.style.display ='flex';
+            
+        }
+        else{
+           
+        }
+    }
+    function HideVideos(){
+       
+        let para=document.getElementById('firstvideos2');
+         if(para.style.display != 'none'){
+             para.style.display ='none';
+             return 0 ;
+         }
+         else{
+        
+         }
+    }
+         
